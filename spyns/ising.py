e9eb7ef717e32c831a70a91b414e1eb20c739852
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-09-07 15:29:58 -0400

    Add spaces, basic linting

--- spyns/ising.py
+++ spyns/ising.py
@@ -37,60 +37,67 @@ class Ising(object):
     coords = []
 
     def __init__(self, n):
+
         self.n = n
-        self.s = [[[1 for x in range(n)] for y in range(n)] for z in range (n)]
+        self.s = [[[1 for x in range(n)] for y in range(n)] for z in range(n)]
         self.magnetization = n**3
-        
+
     def __getitem__(self, point):
+
         n = self.n
-        x,y,z = point
-        return self.s[(x+n)%n][(y+n)%n][(z+n)%n]
-    
+        x, y, z = point
+
+        return self.s[(x + n) % n][(y + n) % n][(z + n) % n]
+
     def __setitem__(self, point, value):
+
         n = self.n
-        x,y,z = point
-        self.s[(x+n)%n][(y+n)%n][(z+n)%n] = value
-    
+        x, y, z = point
+        self.s[(x + n) % n][(y + n) % n][(z + n) % n] = value
+
     def configuration(self, h, t):
+
         n = self.n
-        config = [] 
+        config = []
         for i in range(n):
             for j in range(n):
                 for k in range(n):
-                    cnfg = np.array([i, j, k , self.s[i][j][k]])
+                    cnfg = np.array([i, j, k, self.s[i][j][k]])
                     config.append(cnfg)
-                    
-        tname = "h="+str(h)+"/configurations/t="+str(t)+".txt"
+
+        tname = "h=" + str(h) + "/configurations/t=" + str(t) + ".txt"
         np.savetxt(tname, config)
-    
+
     def step(self, t, h):
+
         n = self.n
-        x,y,z = randint(0,n-1),randint(0,n-1),randint(0,n-1)
-        neighbors =[(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y-1,z),(x,y,z-1),(x,y,z+1)]
-        dE = -2.0*self[x,y,z]*(h+sum(self[xn,yn,zn] for xn, yn, zn in neighbors))
-        if dE > t*math.log(random()):
-           self[x,y,z] = -self[x,y,z]
-           self.magnetization +=2*self[x,y,z]
-           
-        
+        x, y, z = randint(0, n - 1), randint(0, n - 1), randint(0, n - 1)
+        neighbors = [(x - 1, y, z), (x + 1, y, z), (x, y - 1, z),
+                     (x, y - 1, z), (x, y, z - 1), (x, y, z + 1)]
+        dE = -2.0 * self[x, y, z] * \
+            (h + sum(self[xn, yn, zn] for xn, yn, zn in neighbors))
+        if dE > t * math.log(random()):
+            self[x, y, z] = -self[x, y, z]
+            self.magnetization += 2 * self[x, y, z]
+
         return self.magnetization
 
-def main():  
+
+def main():
+
     ising = Ising(n=10)
     steps = 25000
-    for h in range (1,11):
-        data =[]
-        for t in range (1,11):
-            m = [ising.step(t=float(t),h=float(h)) for k in range(steps)]
-            print (h, t)
+    for h in range(1, 11):
+        data = []
+        for t in range(1, 11):
+            m = [ising.step(t=float(t), h=float(h)) for k in range(steps)]
+            print(h, t)
             mu = np.mean(m)
-            sigma = np.std(m) 
-            ising.configuration(t=t,h=h)
-            x= (t, mu, sigma)
-            data.append(x)    
-            #fname = str(t)+".txt"    
+            sigma = np.std(m)
+            ising.configuration(t=t, h=h)
+            x = (t, mu, sigma)
+            data.append(x)
+            #fname = str(t)+".txt"
             #np.savetxt(fname, np.array(m))
-        fname = "h="+str(h)+"/mg.txt"   
-        np.savetxt(fname, data)       
-
-
+        fname = "h=" + str(h) + "/mg.txt"
+        np.savetxt(fname, data)
