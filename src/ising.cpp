bd7f1f79a3fa7b4123fd0e9150f46e578157f58b
ssilayi <31740298+ssilayi@users.noreply.github.com>
2017-09-19 12:02:34 -0400

    fixed issues, updated average value calculations..
    to the running algorithm (improved speed some) and included variance calculations while averaging for additional statistics.

--- src/ising.cpp
+++ src/ising.cpp
@@ -1,27 +1,42 @@
-
+//ising 2.0
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <random>
+
 
 using namespace std;
 
-int N = 10; //lattice size
-int *** si; //spin configuration
-
-double H; //Energy
-double M; //Magnetization
-
-double h; //external field
-double T; //temperature values
-
-int MCSteps = 200000;
-
-void initialize(){
+/* rnd(a,b)
+ * Random number generator 
+ * using the Mersenne Twister
+ * takes in: a - lower limit, real number
+ *           b - upper limit, real number
+ *
+ * returns:  x - random number between a & b
+ */
+double rnd (double a, double b){
     
-    si =new int ** [N];
+    /*http://stackoverflow.com/a/22935167*/
+    random_device rd{};    
+    mt19937 engine{rd()};
+    uniform_real_distribution<double> dist{a, b};
+    double x = dist(engine);
+    return x;
+}
+
+/* initalize(N, si)
+ * Generates initial random spin configuration 
+ * for an N*N*N lattice grid. Each lattice site
+ * is addressed as si[x][y][z] with 0 <= x,y,z < N
+ * takes in: N  - length of one side
+ *           si - 3D lattice of spin sites
+ *
+ */   
+void initialize(int N, int *** si){
     
     for (int x = 0; x < N; x++){
         si[x] = new int * [N];
@@ -30,9 +45,9 @@ void initialize(){
             si[x][y] = new int [N];
             
             for (int z = 0; z < N; z++){
-                
-                double r = (double) rand()/RAND_MAX;
-                si[x][y][z] = 1;
+                double p  = rnd(0.0, 1.0);
+                if (p > 0.5) si[x][y][z] = 1;
+                else si[x][y][z] = -1;
             }
         }
     }
@@ -40,7 +55,16 @@ void initialize(){
     return;
 }
 
-int neighbors(int * point ){
+/* neighbors(N, si, point)
+ * calculates the sum of spins for the 
+ * 6 neighboring sites of a point in the 
+ * grid.
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ *           point - site of interest
+ * returns: sj - sum of the spins of neighbors of point
+ */ 
+int neighbors(int N, int *** si, int * point){
     
     int x = point[0];
     int y = point[1];
@@ -53,27 +77,41 @@ int neighbors(int * point ){
     int zp = (z+N-1) % N;
     int zm = (z+1) % N;
     
-    
-    int sj = si[xp][y][z] + si[xm][y][z] + si[x][yp][z] + si[x][ym][z] + si[x][y][zp] + si[x][y][zm];
+    int sj = si[xp][y][z] + si[xm][y][z] + si[x][yp][z] 
+                      + si[x][ym][z] + si[x][y][zp] + si[x][y][zm];
     
     return sj;
 }
 
-double Magnetization(){
+/* Magnetization(N, si)
+ * sums the spins of each site and returns total
+ * spin/magnetization
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ * returns: M - sum of all spins
+ */
+double Magnetization(int N, int *** si){
     
     double M = 0.0;
     
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             for (int k = 0; k < N; k++)
-                M = M+ (double)si[i][j][k];
-    
-    return M;
-    
+                M = M+(double)si[i][j][k];
+            
+            return M;
+        
 }
 
-
-double Energy(){
+/* Energy(N, si, h, J)
+ * calculates total energy of the grid
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ *           h - external manetic field
+ *           J - nearest neighbor coupling constant
+ * returns: H - total energy of grid
+ */   
+double Energy( int N, int *** si, double h, double J){
     
     double H = 0.0;
     int * point = new int [3];
@@ -83,42 +121,70 @@ double Energy(){
             for (int k = 0; k < N; k++){
                 point[0] = i; point[1] = j; point[2]=k;
                 
-                int sj = neighbors(point);
-                H = H + ( - si[i][j][k]*(h+sj));
+                int sj = neighbors(N, si, point);
+                H = H + ( -(J*si[i][j][k]*sj) - h*si[i][j][k] );
             }
-    
-    free (point);
-    
-    return H / 2.;
+            
+            free (point);
+        
+        return H / 2.0;
 }
 
-double deltaE (int * point){
+/* deltaE(N, si, h, J, point)
+ * calculates the change in energy if a spin site is flipped
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ *           h - external manetic field
+ *           J - nearest neighbor coupling constant
+ *           point - site of interest
+ * returns: dE - change in energy from flipping point
+ */
+double deltaE (int N, int *** si, double h, double J, int * point)
+{
     
-    int sj = neighbors(point);
-    double dE = -2. * si[point[0]][point[1]][point[2]]*(h+sj);
+    int sj = neighbors(N, si , point);
+    double dE = -2.0 * ( (J*si[point[0]][point[1]][point[2]]*sj) 
+    + (h*si[point[0]][point[1]][point[2]]) );
     
-    return dE;
-    
+    return dE;    
 }
 
-void MC_move(){
+/* MC_move(N, si, h, J, M, H, T)
+ * single step Monte Carlo move to determine
+ * spin flip of a random site and update the Magnetization, M,
+ * and energy, H.
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ *           h - external manetic field
+ *           J - nearest neighbor coupling constant
+ *           M - total magnetization
+ *           H - total energy
+ *           T - temperature 
+ */ 
+void MC_move(int N, int *** si, double h, double J, 
+                            double &M, double &H, double T)
+{
     
-    double beta = 1./T;
+    double beta = 1.0/T;
+    
+    int i = (int) rnd(0.0, (double)N)%N; 
+    int j = (int) rnd(0.0, (double)N)%N; 
+    int k = (int) rnd(0.0, (double)N)%N;
+    
     int * point = new int [3];
-    int i = rand()%N; int j = rand()%N; int k = rand()%N;
-    
     point[0] = i; point[1] = j; point[2]=k;
     
+    double dE = deltaE(N, si, h, J, point);
+    double dm = 0.0, dh = 0.0;
     
-    double dE = deltaE(point);
-    double dm=0.0,dh=0.0;
-    double p = T*log((double)rand()/RAND_MAX);
+    double p = T*log(rnd(0.0, 1.0));
+    
     if (dE > p)
     {
         
         si[i][j][k] *= -1;
-        dm=2.*si[i][j][k];
-        dh=dE;
+        dm = 2.0*si[i][j][k];
+        dh = -0.5*dE;
     }
     
     M = M+dm;
@@ -128,79 +194,142 @@ void MC_move(){
     return;
 }
 
-void collect_data(double * data){
-    data[0] = data[0]+M;
-    data[1] = data[1]+H;
-    data[2] = data[2]+M*M;
-    data[3] = data[3]+H*H;
-    data[4] = data[4]+fabs(M);
+
+/* ave_var(cc, Avg, Var, U)
+ * calculates and upates the instantaneous average (Avg) 
+ * and variance (Var) values of a quantity (U) at a time 
+ * step (cc).
+ * takes in: cc  - time step 
+ *           U   - quantity being averaged (M,H, M^2, H^2)
+ *           Avg - variable for the average value 
+ *           Var - variable for the variance 
+ */  
+void ave_var(int cc, double &Avg, double &Var, double U)
+{
+    
+    double old_Avg = Avg;
+    
+    Avg = cc == 0 ? U : Avg + ((U - Avg) / cc);
+    
+    Var = cc == 0 ? 0 : Var * cc + (U - old_Avg) * (U - Avg);
+    Var /= (cc + 1);
 }
 
-void configuration(){
-    
+
+/* configuration(N, si, filename)
+ * saves a snapshot of the configuration
+ * to file named "filename".
+ * takes in: N - length of one side
+ *           si - 3D lattice of spin sites
+ *           filename - name of file to save to
+ */
+void configuration(int N, int *** si, string filename){
     ofstream cfile;
-    std::ostringstream cf;
-    cf <<"h="<<h<<"/configurations/t="<<int(T)<<".txt" ;
-    std::string cnf = cf.str();
+    cfile.open(filename.c_str());
     
-    cfile.open(cnf.c_str());
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             for (int k = 0; k < N; k++)
             {
-                cfile<<i <<"\t" << j << "\t" << k << "\t" << si[i][j][k] << "\n";
+                cfile << i <<"\t" << j << "\t" << k << "\t" 
+                << si[i][j][k] << "\n";
             }
-    
-    return;
-    
+            
+            cfile.close();
+        
+        return;
 }
 
-
+/*main program to calculate M,H values*/
 int main(){
-    h = 1.;
     
-    do{
-        ofstream tfile;
-        std::ostringstream tf;
-        tf <<"h="<<h<<"/mg.txt" ;
-        std::string tmf = tf.str();
-        
-        tfile.open(tmf.c_str());
-
-        
-        for (T=1.0; T <= 12.; T++){
-            
-            initialize();
-            M = Magnetization();
-            H = Energy();
-            
-            double * data;
-            data = new double [5];
-            for (int i=0; i < 5; i++) data[i] = 0.0;
-            
-            for (int step = 0; step < MCSteps; step++){
-                
-                MC_move();
-                collect_data(data);
-            }
-            
-            double norm   = 1.0/(MCSteps*N);
-            double M_avg  = data[0]*norm;
-            double H_avg  = data[1]*norm;
-            double M_2avg = data[2]*norm;
-            double H_2avg = data[3]*norm;
-            double M_abs  = data[4]*norm;
-            
-            tfile << T << "\t" << M_avg << "\t" << H_avg << "\n";
-            configuration();
-        }
-        
-        
-        
-        h=h+1.;
-        
-    }while(h<=10.0);
+    int N = 10;                  //lattice dimension, lenght of single side
+    int MCSteps = 500*N*N*N;    //number of Monte Carlo moves
+    int eqSteps = 0.5*MCSteps;  //number of equilibration steps
     
+    
+    double h = 1.0;             //external magnetic field
+    double J = 2.0;             //coupling constant
+    double T;                   //temperature value
+    
+    //spin sites
+    int *** si = new int ** [N];
+    initialize(N, si);
+    
+    //initial Energy and Magnetization
+    double M = Magnetization(N, si);
+    double H = Energy(N, si, h, J);
+    
+    //Average and Variance 
+    double MAvg, HAvg, MVar, HVar;
+    double M2Avg, H2Avg, M2Var, H2Var;
+    
+    //reassurance
+    cout << "Running h = " << h << "\n";
+    
+    /*open file for writing*/
+    ofstream tfile;
+    std::ostringstream tf;
+    tf <<"h="<<h<<"_mvst.txt" ;
+    std::string tmf = tf.str();
+    tfile.open(tmf.c_str());
+    tfile << "T  \t  <M>  \t  M_Var  \t  <M^2>  \t  M^2_Var \t" 
+    << "<E>  \t  E_Var  \t  <E^2>  \t  E^2_Var \t" 
+    << "X    \t  C_v   \n" ;
+    
+    /*loop over T values*/
+    for (T=1.0; T <= 20.; T+=1){
+        
+        
+        //Equilibration steps (burn-in)
+        for (int step = 0; step < eqSteps; step++)
+            MC_move(N, si, h, J, M, H, T);
+        
+        //Production steps
+        for (int step = 0; step < MCSteps; step++){
+            
+            MC_move(N, si, h, J, M, H, T);
+            
+            //running average and variance calculations 
+            ave_var(step, MAvg, MVar, M);
+            ave_var(step, HAvg, HVar, H);
+            ave_var(step, M2Avg, M2Var, M*M);
+            ave_var(step, H2Avg, H2Var, H*H);
+            
+            
+        }//end loop over MCSteps
+        
+        
+        //Quantities
+        
+        double beta = 1.0/T;
+        double beta2 = beta*beta;
+        double N3 = (double)N*N*N;
+        
+        //Susceptibility
+        double X = beta*N3*( M2Avg - (MAvg*MAvg));
+        
+        //Specific Heat
+        double Cv = (beta2/N3)*(H2Avg - (HAvg*HAvg));
+        
+        /*write to file*/
+        tfile << T << "\t"  
+        << MAvg << "\t" << MVar << "\t" << M2Avg << "\t" << M2Var << "\t" 
+        << HAvg << "\t" << HVar << "\t" << H2Avg << "\t" << H2Var << "\t" 
+        << X    << "\t" << Cv   << "\n" ;
+        
+        //reassure      
+        cout << "Done h = " << h << ", T = " << T <<"\n";
+        
+    }//end for loop over T
+    
+    tfile.close();
+    
+    //reassure
+    cout << "Done h = " << h << "\n";
+    
+    free (si);
     
     return 0;
+    
 }
