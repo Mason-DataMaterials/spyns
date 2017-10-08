8fc1fc7561882115394422c2a7d715c2475d6220
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-10-08 18:55:55 -0400

    Create skeleton file for Markov-Chain Monte Carlo simulator engines

---
+++ spyns/mcmc.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Generic engine for Markov-Chain Monte Carlo simulations plus algorithms.
+
+Copyright (c) Mason DataMaterials Group.
+Distributed under the terms of the MIT License.
+
+"""
+
+import logging
+
+__author__ = "James Glasbrenner"
+__copyright__ = "Copyright 2017, Mason DataMaterials Group"
+__maintainer__ = "James Glasbrenner"
+__email__ = "jglasbr2@gmu.edu"
+__date__ = "October 8, 2017"
+
+logger = logging.getLogger(__name__)
+
+
+class MCMC(object):
+    """Generic class for implementing Markov-Chain Monte Carlo simulations."""
+
+    def __init__(self):
+        """Construct Markov-Chain Monte Carlo simulator."""
+        pass
