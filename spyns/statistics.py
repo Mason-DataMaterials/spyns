64043902e5f9343cd430e7a4c17dcf455dba8f6e
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-10-08 18:56:20 -0400

    Create skeleton file for helper functions that compute statistical quantities.

---
+++ spyns/statistics.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Functions for computing statistics of Markov-Chain Monte Carlo simulations.
+
+Copyright (c) Mason DataMaterials Group.
+Distributed under the terms of the MIT License.
+
+"""
+
+import logging
+
+__author__ = "James Glasbrenner"
+__copyright__ = "Copyright 2017, Mason DataMaterials Group"
+__maintainer__ = "James Glasbrenner"
+__email__ = "jglasbr2@gmu.edu"
+__date__ = "October 8, 2017"
+
+logger = logging.getLogger(__name__)
