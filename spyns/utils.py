c2c5f2aa16629240b9c0b48034d0e713083e5732
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-10-09 00:16:11 -0400

    Add helper function that converts spherical coordinates to cartesian

--- spyns/utils.py
+++ spyns/utils.py
@@ -7,6 +7,7 @@ __email__ = "jglasbr2@gmu.edu"
 __date__ = "September 8, 2017"
 
 import logging
+import math
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
@@ -25,8 +26,43 @@ def debug_logmaker():
         pass
 
     return logging.handlers.RotatingFileHandler(
-        "{0}".format(filename),
-        maxBytes=31457280,
-        backupCount=20,
-        encoding='utf8',
-    )
+        "{0}".format(filename), maxBytes=31457280, backupCount=20,
+        encoding='utf8')
+
+
+def convert_spherical_to_cartesian(rho, phi, theta):
+    """Convert from spherical coordinates into cartesian coordinates.
+
+    Parameters
+    ----------
+    rho : float
+        3D vector length
+
+    phi : float
+        Counterclockwise angle in the xy plane from the x axis (phi = 0) to the
+        xy projection of the 3D vector.
+
+    theta : float
+        The angle between the z axis and the 3D vector.
+
+    Returns
+    -------
+    list
+        The [x, y, z] components corresponding to the vector defined by `rho`,
+        `phi`, and `theta`.
+
+    """
+    # convert phi and theta into radians
+    phi = phi * math.pi / 180
+    theta = theta * math.pi / 180
+
+    # Cartesian coordinates
+    x = rho * math.sin(theta) * math.cos(phi) if abs(rho) >= 1E-9 else 0
+    y = rho * math.sin(theta) * math.sin(phi) if abs(rho) >= 1E-9 else 0
+    z = rho * math.cos(theta) if abs(rho) >= 1E-9 else 0
+
+    return [
+        0 if abs(x) < 1E-9 else x,
+        0 if abs(y) < 1E-9 else y,
+        0 if abs(z) < 1E-9 else z,
+    ]
