57abb9a2ade1c843aed3204dab018b5b9cabb06a
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-10-09 00:18:10 -0400

    Copied over code to use YAML file to create a pymatgen structure object from my command_center code base. Removed references and try/except tests involving parameters and comments. Removed other redundant lines. Currently untested.

--- spyns/data_structures.py
+++ spyns/data_structures.py
@@ -6,12 +6,19 @@ Distributed under the terms of the MIT License.
 
 """
 
+import copy
 import logging
+import operator
+import sys
+from pathlib import Path
 
-import pymatgen as pmg
 import ruamel.yaml
 from monty.json import MSONable
+from pymatgen import Lattice, Structure
+from pymatgen.standard_transformations import SupercellTransformation
 from ruamel.yaml.scanner import ScannerError
+
+from spyns.utils import convert_spherical_to_cartesian
 
 __author__ = "James Glasbrenner"
 __copyright__ = "Copyright 2017, Mason DataMaterials Group"
@@ -25,7 +32,7 @@ logger = logging.getLogger(__name__)
 class SpynsSystem(MSONable):
     """Crystal structure defining a material system."""
 
-    def __init__(self):
+    def __init__(self, pmg_structure):
         """Build SpynsStructure directly from a pymatgen Structure object.
 
         Parameters
@@ -35,7 +42,35 @@ class SpynsSystem(MSONable):
             Structure class.
 
         """
-        pass
+        if pmg_structure and isinstance(pmg_structure, Structure):
+            logger.debug("Input structure appears to be a pymatgen.Structure "
+                         "object.")
+            self.pmg_structure = pmg_structure
+
+        else:
+            logger.debug("Input structure appears to not be a "
+                         "pymatgen.Structure object. Checking if dict-like "
+                         "object.")
+
+            try:
+                pmg_structure.keys()
+                self.pmg_structure = self._build_structure(pmg_structure)
+
+            except AttributeError:
+                logger.debug("Input structure is not a dict-like object. "
+                             "It is instead the type %s. ",
+                             type(pmg_structure))
+                raise
+
+            except TypeError:
+                logger.debug("Input structure is not a dict-like object. "
+                             "It is instead the type %s. ",
+                             type(pmg_structure))
+                raise
+
+            except NameError:
+                logger.debug("Input structure is not defined.")
+                raise
 
     @classmethod
     def from_yaml(cls, filename: str):
@@ -99,7 +134,6 @@ class SpynsSystem(MSONable):
         sublattices. The schematic for defining supercell transformations is:
 
         .. code-block:: yaml
-           structure:
            transformations:
              primitive: bool
              matrix:
@@ -138,7 +172,250 @@ class SpynsSystem(MSONable):
             An instance of the SpynsStructure class
 
         """
-        pass
+        try:
+            logger.debug("Reading file = %s", filename)
+            yaml = ruamel.yaml.YAML()
+            yaml_dict = yaml.load(Path(filename))
+            spyns_structure = cls.build_structure(yaml_dict)
+            logger.debug("spyns_structure = %s", spyns_structure)
+
+        except TypeError:
+            logger.debug("Reading dict-like object = %s", filename)
+            yaml_dict = copy.deepcopy(filename)
+            spyns_structure = cls.build_structure(yaml_dict)
+            logger.debug("spyns_structure = %s", spyns_structure)
+
+        except ScannerError:
+            logger.error("Input is not a YAML file or dict-like object!")
+            raise
+
+        return cls(pmg_structure=spyns_structure)
+
+    @staticmethod
+    def build_structure(structure_parameters):
+        """Placeholder."""
+        cls = SpynsSystem
+
+        try:
+            input_method = structure_parameters.get('input')
+            site_properties = structure_parameters.get('site_properties')
+            transformations = structure_parameters.get('transformations')
+
+        except AttributeError:
+            logger.error("Structure parameters information is not "
+                         "dict-like.")
+            raise
+
+        if input_method == "spacegroup":
+            try:
+                structure = cls.from_spacegroup_to_structure(
+                    sg=structure_parameters["sg"],
+                    crystal_system=(
+                        structure_parameters["lattice"]["crystal_system"]),
+                    lattice_parameters=(
+                        structure_parameters["lattice"]["parameters"]),
+                    species=structure_parameters["species"],
+                    coords=structure_parameters["coords"],
+                    site_properties=site_properties, )
+
+            except TypeError:
+                logger.error("At least one of the structure spacegroup "
+                             "parameters is an incorrect type.", exc_info=True)
+                raise
+
+            except KeyError:
+                logger.error("At least one of the structure spacegroup "
+                             "parameters is missing.", exc_info=True)
+                raise
+
+        elif input_method == "file":
+            try:
+                structure_filepath = structure_parameters["file"]
+                structure = Structure.from_file(structure_filepath)
+
+            except FileNotFoundError:
+                logger.error("Input file not found.", exc_info=True)
+                raise
+
+            except KeyError:
+                logger.error("The structure filepath parameter is not found.",
+                             exc_info=True)
+                raise
+
+            if site_properties:
+                try:
+                    logger.debug("Site-properties pre-formatting: "
+                                 "%s", site_properties)
+                    cls.format_site_properties(site_properties)
+                    logger.debug("Site-properties post-formatting: "
+                                 "%s", site_properties)
+                    cls.set_site_properties(structure, site_properties)
+
+                except TypeError:
+                    logger.error("At least one of the site properties is not "
+                                 "structured correctly.", exc_info=True)
+                    raise
+
+                except AttributeError:
+                    logger.error("Site properties need to be in a dict-like "
+                                 "format.", exc_info=True)
+                    raise
+
+        else:
+            logger.debug("The specified input method \"%s\" is invalid.",
+                         input_method)
+            sys.exit("Invalid input method")
+
+        if transformations:
+            structure = cls.apply_structure_transformations(
+                structure, transformations)
+
+        return structure
+
+    @staticmethod
+    def from_spacegroup_to_structure(sg, crystal_system, lattice_parameters,
+                                     species, coords, site_properties=None):
+        """Placeholder."""
+        cls = SpynsSystem
+
+        lattice = getattr(Lattice, crystal_system)
+
+        try:
+            logger.debug("Site-properties pre-formatting: "
+                         "%s", site_properties)
+            cls.format_site_properties(site_properties)
+            logger.debug("Site-properties post-formatting: "
+                         "%s", site_properties)
+
+        except AttributeError:
+            logger.debug("site_properties is not a dict-like object. "
+                         "Structure object will not have site properties.")
+
+        structure = Structure.from_spacegroup(
+            sg=sg,
+            lattice=lattice(**lattice_parameters),
+            species=species,
+            coords=coords,
+            site_properties=site_properties, )
+
+        return structure
+
+    @staticmethod
+    def set_site_properties(structure, site_properties):
+        """Placeholder."""
+        for property_name, site_values in site_properties.items():
+            structure.add_site_property(property_name, site_values)
+
+    @staticmethod
+    def format_site_properties(site_properties):
+        """Placeholder."""
+        copy_of_site_properties = copy.deepcopy(site_properties)
+
+        for property_name, sites in copy_of_site_properties.items():
+            list_of_site_values = []
+
+            for site_index, site_value in sites.items():
+                if property_name == "magmom":
+                    if all([
+                            component_name in dict(site_value)
+                            for component_name in ["x", "y", "z"]
+                    ]):
+                        x = site_value["x"]
+                        y = site_value["y"]
+                        z = site_value["z"]
+
+                        components = [x, y, z]
+
+                        list_of_site_values.append([site_index, components])
+
+                    elif all([
+                            component_name in dict(site_value)
+                            for component_name in ["rho", "phi", "theta"]
+                    ]):
+                        rho = site_value["rho"]
+                        phi = site_value["phi"]
+                        theta = site_value["theta"]
+
+                        components = convert_spherical_to_cartesian(
+                            rho=rho, phi=phi, theta=theta)
+
+                        list_of_site_values.append([site_index, components])
+
+                    else:
+                        logger.error("Unrecognized components for magmom.")
+                        sys.exit("Unrecognized components for magmom.")
+
+                elif property_name == "sublattice":
+                    list_of_site_values.append([site_index, site_value])
+
+                else:
+                    logging.error("Unsupported property name %s",
+                                  property_name)
+                    sys.exit("Unsupported property name.")
+
+            # Sort the list by site index
+            sorted(list_of_site_values, key=operator.itemgetter(0))
+            logger.debug("Site values for %s property: "
+                         "%s", property_name, list_of_site_values)
+
+            # Overwrite property_name dict with structured list
+            site_properties[property_name] = [
+                site[1] for site in list_of_site_values
+            ]
+
+    @staticmethod
+    def apply_structure_transformations(structure, transformations):
+        """Placeholder."""
+        working_structure = copy.deepcopy(structure)
+        primitive = transformations.get("primitive")
+        supercell = transformations.get("supercell")
+
+        if primitive:
+            working_structure = working_structure.get_primitive_structure()
+
+        if supercell:
+            supercell_transform = supercell.get("transform")
+            supercell_scaling = supercell.get("scaling")
+
+            if supercell_transform:
+                scaling_matrix = supercell_transform.get("scaling_matrix")
+
+                transformation = (SupercellTransformation(
+                    scaling_matrix=scaling_matrix))
+                logger.debug("SupercellTransformation scaling matrix is "
+                             "%s", transformation.scaling_matrix)
+                working_structure = (
+                    transformation.apply_transformation(working_structure))
+
+            if supercell_scaling:
+                scale_a = supercell_scaling["a"]
+                scale_b = supercell_scaling["b"]
+                scale_c = supercell_scaling["c"]
+
+                supercell_scaling_transformation = (
+                    SupercellTransformation.from_scaling_factors(
+                        scale_a=scale_a, scale_b=scale_b, scale_c=scale_c))
+                working_structure = (supercell_scaling_transformation.
+                                     apply_transformation(working_structure))
+
+        return working_structure
+
+    def as_dict(self):
+        """Convert SpynsSystem into a serialized JSON-style dict."""
+        dict_spyns_system = {"spyns_system": self.pmg_structure.as_dict()}
+
+        # MSONable boilerplate
+        dict_spyns_system["@module"] = self.__class__.__module__
+        dict_spyns_system["@class"] = self.__class__.__name__
+
+        return dict_spyns_system
+
+    @classmethod
+    def from_dict(cls, dict_spyns_system):
+        """Regenerate SpynsSystem using serialized JSON-style dict."""
+        spyns_system = dict_spyns_system.get("spyns_system")
+
+        return cls(pmg_structure=pmg.Structure.from_dict(spyns_system))
 
 
 class SimulationData(MSONable):
