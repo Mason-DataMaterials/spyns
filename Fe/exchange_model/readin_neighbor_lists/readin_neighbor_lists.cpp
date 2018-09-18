646b6237eaf74c52431792e10d279d2679752a7d
ssilayi <ssilayi@gmu.edu>
2018-09-18 15:54:05 -0400

    updating code to read in neighbor lists for notebook output

---
+++ Fe/exchange_model/readin_neighbor_lists/readin_neighbor_lists.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    
+    
+
+      
+    
+  string line;
+  ifstream infile ("neighbor_lists.txt");
+  
+  int N;
+  int num_1st_nn,num_2nd_nn;
+  
+  if (infile.is_open())
+  {
+   
+      infile>>N;
+      infile>>num_1st_nn>>num_2nd_nn;
+      
+      //populate neighbor lists
+      double **first_nn = new double * [N];
+      double **second_nn = new double * [N];
+      for (int i = 0; i < N; i++)
+      {
+          first_nn[i] = new double [num_1st_nn];
+          second_nn[i] = new double [num_2nd_nn];
+      }  
+      
+      int nn_per_site = num_1st_nn + num_2nd_nn;
+      
+      for (int i = 0; i < N; i++){
+          cout << i << "\t"; 
+          
+          for (int j = 0; j < num_1st_nn; j++){
+              infile >> first_nn[i][j]; 
+              
+              cout << first_nn[i][j] << "\t";
+          }
+          
+          for (int j = 0; j < num_2nd_nn; j++){
+              infile >> second_nn[i][j]; 
+              cout << second_nn[i][j] << "\t";
+          }
+          
+          
+          cout << "\n";
+      }
+        
+       
+      
+      infile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
